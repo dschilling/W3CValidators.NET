f70c42fb5a4b601d81bcbed9a6ac1090a8644dcb
dschilling <daniel@raw-data.org>
2009-08-12 17:57:20 -0400

    All tests pass.

--- src/W3CValidators/Markup/MarkupValidatorClient.cs
+++ src/W3CValidators/Markup/MarkupValidatorClient.cs
@@ -4,9 +4,9 @@ namespace W3CValidators.Markup
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Net;
     using System.Web;
-    using System.Xml;
 
     public class MarkupValidatorClient
     {
@@ -19,21 +19,68 @@ namespace W3CValidators.Markup
 
         public MarkupValidatorResponse Check(Uri documentUri, MarkupValidatorOptions options)
         {
-            var queryStrings = (options ?? new MarkupValidatorOptions())
-                .ToDictionary();
+            if (options == null)
+                options = new MarkupValidatorOptions();
+
+            var queryStrings = options.ToDictionary();
             queryStrings.Add("uri", documentUri.ToString());
             var request = WebRequest.Create(AppendQueryString(_validator, queryStrings));
+
             return ParseResponse(request);
         }
 
         public MarkupValidatorResponse Check(byte[] documentData, MarkupValidatorOptions options)
         {
-            throw new NotImplementedException();
+            if (options == null)
+                options = new MarkupValidatorOptions();
+
+            var request = this.ConstructPostRequest(
+                options,
+                writer => writer.Write("uploaded_file", "document.html", "text/html", documentData));
+
+            return ParseResponse(request);
         }
 
         public MarkupValidatorResponse Check(string documentFragment, MarkupValidatorOptions options)
         {
-            throw new NotImplementedException();
+            if (options == null)
+                options = new MarkupValidatorOptions();
+
+            var request = this.ConstructPostRequest(
+                options,
+                writer => writer.Write("fragment", documentFragment));
+
+            return ParseResponse(request);
+        }
+
+        private WebRequest ConstructPostRequest(MarkupValidatorOptions options, Action<MultipartFormDataWriter> writePayload)
+        {
+            var request = WebRequest.Create(this._validator);
+            var boundary = Guid.NewGuid().ToString();
+            request.Method = "POST";
+            request.ContentType = string.Concat("multipart/form-data; boundary=", boundary);
+
+            using (var contents = new MemoryStream())
+            {
+                using (var writer = new MultipartFormDataWriter(contents, boundary))
+                {
+                    foreach (var pair in options.ToDictionary())
+                    {
+                        writer.Write(pair.Key, pair.Value);
+                    }
+
+                    writePayload(writer);
+                }
+
+                contents.Flush();
+                request.ContentLength = contents.Length;
+
+                using (var requestStream = request.GetRequestStream())
+                {
+                    contents.WriteTo(requestStream);
+                }
+            }
+            return request;
         }
 
         private static Uri AppendQueryString(Uri baseUri, ICollection<KeyValuePair<string, string>> queryStrings)
---
+++ src/W3CValidators/Markup/MultipartFormDataWriter.cs
@@ -0,0 +1,68 @@
+namespace W3CValidators.Markup
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    internal class MultipartFormDataWriter : IDisposable
+    {
+        private readonly Stream _stream;
+        private readonly string _boundary;
+        private readonly string _header;
+        private readonly string _footer;
+
+        public MultipartFormDataWriter(Stream stream, string boundary)
+        {
+            _stream = stream;
+            _boundary = boundary;
+
+            _header = string.Concat("--", _boundary);
+            _footer = string.Concat("--", _boundary, "--");
+        }
+
+        public void Dispose()
+        {
+            this.WriteLine(_footer);
+        }
+
+        public void Write(string name, string value)
+        {
+            this.WriteLine(_header);
+            this.WriteLine(string.Concat("Content-Disposition: form-data; name=\"", name, "\""));
+            this.WriteLine();
+            this.WriteLine(value);
+        }
+
+        public void Write(string name, string filename, string contentType, byte[] data)
+        {
+            this.WriteLine(_header);
+            this.WriteLine(string.Concat("Content-Disposition: form-data; name=\"", name, "\"; filename=\"", filename, "\""));
+            this.WriteLine(string.Concat("Content-Type: ", contentType));
+            this.WriteLine();
+            this.Write(data);
+            this.WriteLine();
+        }
+
+        private void Write(byte[] value)
+        {
+            _stream.Write(value, 0, value.Length);
+        }
+
+        private void Write(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+            this.Write(bytes);
+        }
+
+        private void WriteLine()
+        {
+            this.Write("\r\n");
+        }
+
+        private void WriteLine(string value)
+        {
+            this.Write(value);
+            this.WriteLine();
+        }
+    }
+}
