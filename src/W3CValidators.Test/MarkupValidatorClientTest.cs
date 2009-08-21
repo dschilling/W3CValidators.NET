cfed6e9dcaeedfc8360adfa5cf9c68863d177f15
dschilling <daniel@raw-data.org>
2009-08-21 13:38:38 -0400

    Tests now write xml output.

--- src/W3CValidators.Test/MarkupValidatorClientTest.cs
+++ src/W3CValidators.Test/MarkupValidatorClientTest.cs
@@ -115,7 +115,7 @@ namespace W3CValidators.Test
                     // If the assertion fails, an exception will be thrown and we will never
                     // execute the 2nd "too early" request.  In other words, even if our code is
                     // broken, this test will not abuse the w3c validator service.
-                    Assert.That(stopWatch.ElapsedMilliseconds, Is.GreaterThanOrEqualTo(1000));
+                    Assert.That(stopWatch.ElapsedMilliseconds, Is.GreaterThanOrEqualTo(950)); // about 50 ms of grace
                 }
                 requestCount[0]++;
             };
